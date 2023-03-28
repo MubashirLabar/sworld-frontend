bb177cc374645658d771821267ea144df22dc251
MubashirLabar <61319180+MubashirLabar@users.noreply.github.com>
2023-03-29 03:16:48 +0500

    added hearder inside api

--- src/store/services/chatServices.js
+++ src/store/services/chatServices.js
@@ -6,12 +6,9 @@ const chatServices = createApi({
   tagTypes: "chat",
   baseQuery: fetchBaseQuery({
     baseUrl: env.BASE_URL,
-    // prepareHeaders: (headers, { getState }) => {
-    //   const reducers = getState();
-    //   const token = reducers?.authReducer?.adminToken;
-    //   headers.set("authorization", token ? `Bearer ${token}` : "");
-    //   return headers;
-    // },
+    headers: {
+      "Content-Type": "application/json",
+    },
   }),
 
   endpoints: (builder) => {
@@ -26,66 +23,9 @@ const chatServices = createApi({
         },
         invalidatesTags: ["chat"],
       }),
-
-      //   updateCategory: builder.mutation({
-      //     query: (data) => {
-      //       return {
-      //         url: `/update-category/${data.id}`,
-      //         method: "PUT",
-      //         body: { name: data.name },
-      //       };
-      //     },
-      //     invalidatesTags: ["categories"],
-      //   }),
-
-      //   deleteCategory: builder.mutation({
-      //     query: (id) => {
-      //       return {
-      //         url: `/delete-category/${id}`,
-      //         method: "DELETE",
-      //       };
-      //     },
-      //     invalidatesTags: ["categories"],
-      //   }),
-
-      //   get: builder.query({
-      //     query: (page) => {
-      //       return {
-      //         url: `categories/${page}`,
-      //         method: "GET",
-      //       };
-      //     },
-      //     providesTags: ["categories"],
-      //   }),
-
-      //   fetchCategory: builder.query({
-      //     query: (id) => {
-      //       return {
-      //         url: `/fetch-category/${id}`,
-      //         method: "GET",
-      //       };
-      //     },
-      //     providesTags: ["categories"],
-      //   }),
-
-      //   allCategories: builder.query({
-      //     query: () => {
-      //       return {
-      //         url: "allcategories",
-      //         method: "GET",
-      //       };
-      //     },
-      //   }),
     };
   },
 });
 
-export const {
-  useCreateMutation,
-  //   useGetQuery,
-  //   useFetchCategoryQuery,
-  //   useUpdateCategoryMutation,
-  //   useDeleteCategoryMutation,
-  //   useAllCategoriesQuery,
-} = chatServices;
+export const { useCreateMutation } = chatServices;
 export default chatServices;
